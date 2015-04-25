cf7380422d49cba0a690ec3d480eea2805ec76f2
Cara Hill <chilly23@gmail.com>
2015-04-25 15:10:12 +1200

    Index page now shows a list of all customers.

--- rb/public/js/application.js
+++ rb/public/js/application.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+   var customers = []
+   // var $customers = $()
+
+    $.ajax({
+      url: "/customers",
+      type: 'GET',
+      success: function(data) {
+        for(var i = 0; i < data.length; i++) {
+          var customer = data[i]
+          var $customerName = $('<li>'+customer.name+'</li>');
+          $('#crm').append($customerName);
+          // debugger
+        }
+       // console.log(customers);
+      // $()
+      }
+    })
+
+
 });
