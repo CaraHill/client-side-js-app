d3413fa5d13eb2081df57b6429f814c0575b88a5
Cara Hill <chilly23@gmail.com>
2015-04-25 16:33:16 +1200

    Can now delete customers.

--- rb/public/js/application.js
+++ rb/public/js/application.js
@@ -1,22 +1,39 @@
 $(document).ready(function() {
 
-   var customers = []
-   // var $customers = $()
-
     $.ajax({
       url: "/customers",
       type: 'GET',
       success: function(data) {
         for(var i = 0; i < data.length; i++) {
           var customer = data[i]
-          var $customerName = $('<li>'+customer.name+'</li>');
-          $('#crm').append($customerName);
-          // debugger
+          var customerId = customer.id
+          var customerDiv = '<div class="customer" data-customer-id="'+customerId+'">'+customer.name+
+           '<a class="delete-button" href="">Delete</a> '+
+          '<a class="more-button" href="">More</a>'+ '</div>';
+          $('#crm').append(customerDiv);
         }
-       // console.log(customers);
-      // $()
+      },
+      failure: function() {
+        alert("Where are the customers?!?");
       }
-    })
+    });
+
+    $('#crm').on('click', '.delete-button', function(e) {
+      e.preventDefault();
+      var customer = $(e.target).parent();
+      var customerId = customer.data('customer-id');
+      $.ajax({
+        url: "/customers/"+customerId,
+        type: 'DELETE',
+        success: function() {
+          $(customer).hide();
+        },
+        failure: function() {
+          alert("That customer was not deleted!");
+        }
+      });
+    });
+
 
 
 });
