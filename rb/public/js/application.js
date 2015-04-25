49e72695ec4240abf6b86e61c0684bccf413cff9
Cara Hill <chilly23@gmail.com>
2015-04-25 22:47:55 +1200

    Moved all the event handlers into view prototype functions.

--- rb/public/js/application.js
+++ rb/public/js/application.js
@@ -2,36 +2,15 @@ $(document).ready(function() {
 
   var view = new CrmView('#crm');
   var model = new CrmModel();
+
   model.getAllCustomers(view.allCustomers,view.allCustomersFailure);
-  $('#crm').on('click', '.delete-button', function(e) {
-    e.preventDefault();
-    var customer = $(e.target).parent();
-    var customerId = customer.data('customer-id');
-    model.deleteCustomer(view.deleteCustomer, view.deleteCustomerFailure, customer, customerId);
-  });
 
-  $('#crm').on('click', '.more-button', function(e) {
-    e.preventDefault();
-    var customer = $(e.target).parent();
-    var customerId = customer.data('customer-id');
-    var customerNotes = customer.find('.notes-go-here');
-    model.seeCustomerNotes(view.customerNotes, view.customerNotesFailure, customerId, customerNotes);
-  });
+  view.registerDeleteEventHandler(model.deleteCustomer);
 
-  $('#crm').on('click', '.add-notes', function(e) {
-    e.preventDefault();
-    var customer = $(e.target).parent();
-    var customerId = customer.data('customer-id');
-    var customerForm = customer.find('.new-note').show();
-  });
+  view.registerNotesEventHandler(model.seeCustomerNotes);
 
-  $('#crm').on('submit', '.new-note', function(e) {
-    e.preventDefault();
-    var noteForm = $(e.target)
-    var customer = noteForm.parent();
-    var customerId = customer.data('customer-id');
-    var customerNote = customer.find('.notes-go-here');
-    model.addNewCustomerNote(view.addCustomerNote, view.addCustomerNoteFailure, noteForm, customerId, customerNote);
-  });
+  view.registerAddNotesFormEventHandler();
+
+  view.registerNewNoteEventHandler(model.addNewCustomerNote);
 
 });
--- rb/public/js/views/crm-view.js
+++ rb/public/js/views/crm-view.js
@@ -1,12 +1,42 @@
 function CrmView (rootSelector) {
   this.crm = $(rootSelector);
-
-  this.registerEventHandlers();
 }
 
 CrmView.prototype = {
-  registerEventHandlers: function() {
-
+  registerDeleteEventHandler: function(deleteCustomer) {
+    this.crm.on('click', '.delete-button', function(e) {
+      e.preventDefault();
+      var customer = $(e.target).parent();
+      var customerId = customer.data('customer-id');
+      deleteCustomer(this.deleteCustomer, this.deleteCustomerFailure, customer, customerId);
+    }.bind(this));
+  },
+  registerNotesEventHandler: function(seeCustomerNotes) {
+    this.crm.on('click', '.more-button', function(e) {
+      e.preventDefault();
+      var customer = $(e.target).parent();
+      var customerId = customer.data('customer-id');
+      var customerNotes = customer.find('.notes-go-here');
+      seeCustomerNotes(this.customerNotes, this.customerNotesFailure, customerId, customerNotes);
+    }.bind(this));
+  },
+  registerAddNotesFormEventHandler: function() {
+    this.crm.on('click', '.add-notes', function(e) {
+      e.preventDefault();
+      var customer = $(e.target).parent();
+      var customerId = customer.data('customer-id');
+      var customerForm = customer.find('.new-note').show();
+    });
+  },
+  registerNewNoteEventHandler: function(addNewNote) {
+    this.crm.on('submit', '.new-note', function(e) {
+        e.preventDefault();
+        var noteForm = $(e.target)
+        var customer = noteForm.parent();
+        var customerId = customer.data('customer-id');
+        var customerNote = customer.find('.notes-go-here');
+        addNewNote(this.addCustomerNote, this.addCustomerNoteFailure, noteForm, customerId, customerNote);
+      }.bind(this));
   },
   allCustomers: function(data) {
     for(var i = 0; i < data.length; i++) {
