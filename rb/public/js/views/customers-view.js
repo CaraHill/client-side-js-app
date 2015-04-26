c9c2005915d7ba04ba2104aa16af5e5ff92f900d
Cara Hill <chilly23@gmail.com>
2015-04-26 15:41:59 +1200

    Added a bit of CSS to the page.

--- rb/public/js/views/customers-view.js
+++ rb/public/js/views/customers-view.js
@@ -7,9 +7,14 @@ CustomersView.prototype = {
     for(var i = 0; i < data.length; i++) {
       var customer = data[i]
       var customerId = customer.id
-      var customerDiv = '<div class="customer" data-customer-id="'+customerId+'">'+customer.name+
-       '<a class="delete-button" href="">Delete</a> '+
-      '<a class="more-button" href="">More Info</a> '+'<a class="add-notes" href="">Add New Note</a>'+'<form class="new-note" method="post" action="/customers/"'+customerId+'"/notes"><textarea name="note[content]" placeholder="note content"></textarea><input type="submit" value="submit new note"></form>'+'<div class="notes-go-here"></div>'+'</div>';
+      var customerDiv = '<div class="customer" data-customer-id="'+customerId+'">'
+        +'<h4>'+customer.name+'</h4>'
+        +'<a class="delete-button" href="">Delete</a> '
+        +'<a class="more-button" href="">More Info</a> '
+        +'<a class="add-notes" href="">Add New Note</a>'
+        +'<form class="new-note" method="post" action="/customers/"'+customerId+'"/notes"><textarea name="note[content]" placeholder="note content"></textarea><input type="submit" value="submit new note"></form>'
+        +'<div class="notes-go-here"></div>'
+        +'</div>';
       $('#crm').append(customerDiv);
     }
   },
